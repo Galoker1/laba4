f20d77e5d3fa921f942fea9987e3b8ed51e5eeae
Ga1oker <54763540+Galoker1@users.noreply.github.com>
2020-12-25 23:44:31 +0300

    Add files via upload

--- laba-4/1.js
+++ laba-4/1.js
@@ -1,42 +1,49 @@
 //1
-alert("Hello, World!")
+alert("Hello, World!");
 //2
-let a= 4
-let b=5
-alert(a+b)
+let a= 4;
+let b=5;
+alert(a+b);
 //3
-const c=5
-let d= 6
-let e=4
-alert(c+d+e)
+const c=5;
+let d= 6;
+let e=4;
+alert(c+d+e);
 //4
-let f=2
-let g='Привет'
-alert(g/f)
+let f=2;
+let g='Привет';
+alert(g/f);
 //5
-let str1='Привет'
-let str2=', м'
-let str3='ир!'
-alert(str1+str2+str3)
+let str1='Привет';
+let str2=', м';
+let str3='ир!';
+alert(str1+str2+str3);
 //6
-let x = null
-alert(x)
+let x = null;
+alert(x);
 //7
-let y = false
-alert(y + " "+ typeof y)
-y=String(y)
-alert(y + " "+ typeof y)
+let y = false;
+alert(y + " "+ typeof y);
+y=String(y);
+alert(y + " "+ typeof y);
 
-let mk="1000"
-let nk="10"
-alert(mk/nk)
-let z="777"
-let k=333
-alert(k+z)
+let mk="1000";
+let nk="10";
+alert(mk/nk);
+let z="777";
+let k=333;
+alert(k+z);
 //8
-let ak=2
-let bk=4
-let ck=2
-if (bk*bk-4*ak*ck >0) alert(((-bk+Math.sqrt(bk*bk-4*ak*ck))/2*ak) + '  ' +(( -bk-Math.sqrt(bk*bk-4*ak*ck))/2*ak))
-if (bk*bk-4*ak*ck ==0) alert((-bk/2*ak))
-else alert("Корней нет")
+let ak=2;
+let bk=12;
+let ck=2;
+let dk=bk*bk-4*ak*ck;
+if (dk >0) {
+	alert(((-bk+Math.sqrt(dk))/(2*ak)) + '  ' +(( -bk-Math.sqrt(dk))/(2*ak)));
+	}
+else if (dk == 0) {
+	alert(-bk/(2*ak));
+	}
+else {
+	alert("Корней нет");
+	}
--- laba-4/2.js
+++ laba-4/2.js
@@ -1,59 +1,59 @@
 
 //9
-console.log('9)')
-let x0= prompt( 'x0=')
-let xk = prompt('xk=')
+console.log('9)');
+let x0= prompt( 'x0=');
+let xk = prompt('xk=');
 for (let i = x0;i<=xk;i++){console.log('x='+ String(i) + '   y=' + String(i*i*i+i*i-3*i))}
 
 //10
-console.log('10)')
-let a=[]
-let s=0
+console.log('10)');
+let a=[];
+let s=0;
 for (let k =0;k<5;k++){
-  a[k]=Math.random() * (10 - 1) + 1
-  s+=Math.pow(a[k],2)
+  a[k]=Math.random() * (10 - 1) + 1;
+  s+=Math.pow(a[k],2);
 }
-  console.log('['+a+']' + '   '  + s)
+  console.log('['+a+']' + '   '  + s);
 //11
-console.log('11)')
-let x10= prompt( 'x10=')
-console.log(parseInt(x10).toString(2))
+console.log('11)');
+let x10= prompt( 'x10=');
+console.log(parseInt(x10).toString(2));
 //12
-console.log('12)')
+console.log('12)');
 let b = ['aaa', 'bab', 'aba'];
 let bstring = b.toString();
-let sa=0
+let sa=0;
 for(i = 0; i < bstring.length; i++) {
   if (bstring[i]=='a'){
-  	sa++
+  	sa++;
   }
 }
-console.log(sa)
+console.log(sa);
 //13
-console.log("13)")
+console.log("13)");
 let str = 'Иванов Иван, Петров Пётр, Ульянова Ульяна';
 
 let names = str.split(',');
-console.log(names)
+console.log(names);
 //14
-console.log("14)")
+console.log("14)");
 function funkcia(stroka) {
-	console.log(stroka)
+	console.log(stroka);
 }
-funkcia('Hello, World!')
+funkcia('Hello, World!');
 //15
-console.log("15)")
-let z = "Hello"
+console.log("15)");
+let z = "Hello";
 function change() {
-	let z = "Ugu"
-	alert(z)
+	let z = "Ugu";
+	alert(z);
 }
-change()
-alert(z)
+change();
+alert(z);
 //16
-console.log("16)")
+console.log("16)");
 function summ(x1,x2) {
-	return(x1+x2)
+	return(x1+x2);
 }
-console.log(summ(1,2))
+console.log(summ(1,2));
 
